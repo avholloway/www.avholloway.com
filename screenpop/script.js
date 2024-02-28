abc11107b1865128d434d7c38925af3ff36a56f1
Anthony Holloway <avholloway@gmail.com>
2024-02-28 16:49:37 -0600

    parsing the variable

--- screenpop/script.js
+++ screenpop/script.js
@@ -1,5 +1,10 @@
 const searchParams = new URLSearchParams(window.location.search);
 
+let data = {};
 for (const p of searchParams) {
+  if (p[0] !== "target" && p[0] !== "mask") {
+    data = { key: p[0], value: [1] };
+  }
   console.log(p);
 }
+console.log(data);
