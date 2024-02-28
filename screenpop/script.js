7a8e51fe2a164cb4c6d5f2c2228f7b93dbe4e5dd
Anthony Holloway <avholloway@gmail.com>
2024-02-28 17:00:38 -0600

    added length

--- screenpop/script.js
+++ screenpop/script.js
@@ -6,7 +6,7 @@ for (const p of searchParams) {
     data = { key: p[0], value: p[1] };
   }
 }
-data.masked_value = data.value.slice(-1 * searchParams.get("mask"));
+data.masked_value = data.value.slice(-1 * searchParams.get("mask").length);
 window.location.href = `${searchParams.get("target")}?${data.key}=${
   data.masked_value
 }`;
