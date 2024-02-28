3142db6d42a46ca3a41fb77ba9c168a7a74c84cf
Anthony Holloway <avholloway@gmail.com>
2024-02-28 16:58:38 -0600

    alpha testing

--- screenpop/script.js
+++ screenpop/script.js
@@ -3,8 +3,10 @@ const searchParams = new URLSearchParams(window.location.search);
 let data = {};
 for (const p of searchParams) {
   if (p[0] !== "target" && p[0] !== "mask") {
-    data = { key: p[0], value: [1] };
+    data = { key: p[0], value: p[1] };
   }
-  console.log(p);
 }
-console.log(data);
+data.masked_value = data.value.slice(-1 * searchParams1.get("mask"));
+window.location.href = `${searchParams1.get("target")}?${data.key}=${
+  data.masked_value
+}`;
